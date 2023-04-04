e76eef0af4cae507b81afb6b5e2859cda2187b10
jinnnii <jinnnii@naver.com>
2023-04-04 16:42:47 +0900

    #1 - 스프링부트 프로젝트 초기화
    스프링 부트 2.7.8 뼈대를 세움
    개발 준비 완료

---
+++ src/test/java/com/fastcampus/backendboardadmin/FastcampusBackendBoardAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fastcampus.backendboardadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class FastcampusBackendBoardAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
